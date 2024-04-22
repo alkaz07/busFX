64789ea7fe27707024cf067a8d4434fec29c1c53
alkaz07 <alkaz07@yandex.ru>
2024-04-22 11:09:30 +0300

    Initial commit

---
+++ src/main/java/com/example/busfx/HelloController.java
@@ -0,0 +1,23 @@
+package com.example.busfx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class HelloController {
+    @FXML
+    TextField kolvo;
+    @FXML
+    TextField vysoty;
+
+    @FXML
+    Label crash;
+
+    @FXML
+    Label noCrash;
+
+    @FXML
+    protected void mainCheck() {
+
+    }
+}
